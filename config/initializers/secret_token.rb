ba224d8c3e9881f058f954e6fd4f0bbccd2c6e88
Deke Brown <dknbrown@gmail.com>
2011-05-05 13:31:22 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedBreast::Application.config.secret_token = 'e59b7d61b164ae5830d6b0f38af4787fb7a4a7b9647202ab698119f7b4d211c243533e61698ba8799278385d565eea3e5e1233bf5435f9496feb0b803801d0ca'
