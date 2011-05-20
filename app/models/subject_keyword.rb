d184b1affb9ab57e400fad673ce7cf9b4317f32c
Deke Brown <dknbrown@gmail.com>
2011-05-20 13:14:18 -0600

    fix db relationship

--- app/models/subject_keyword.rb
+++ app/models/subject_keyword.rb
@@ -13,7 +13,7 @@
 class SubjectKeyword < ActiveRecord::Base
 	attr_accessible :keyword, :ep
 	has_many :youtube_feed_entries
-	has_many :youtube_feed_entries
+	has_many :flickr_feeds
 		
 	def self.all_by_ep(ep_num)
 		keys = self.find(:all, :conditions => { :ep => ep_num })
