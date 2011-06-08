ed1e6c7a9e8f6e85c3ba27a2187ea641c36e57dc
Deke Brown <dknbrown@gmail.com>
2011-06-08 13:03:51 -0600

    change twtter-widget

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,20 +4,23 @@ require 'open-uri'
 
 class PagesController < ApplicationController
   
-  
+  EPISODE = 1
   WEATHER_WO_ID = 864342
   def home
     @title = "Looking at stuff"
     
-    @twitter_items = TwitterFeedEntry.all(:limit => 1, :order => "created_at desc")
+    
 
     #@rssfeed_items = RssFeedEntry.all(:limit => 1)
     
     @yt_items = YoutubeFeedEntry.random
     
     @weather_items = update_weather( WEATHER_WO_ID )
+    
+    @tkw = twiter_keywords
 
 	unless SubjectKeyword.nil?
+		@keywords =  SubjectKeyword.all_by_ep(EPISODE)
 		@curkeyword = SubjectKeyword.where("id = ?" , @yt_items.subject_keyword ).first
 	end
 	
@@ -25,7 +28,16 @@ class PagesController < ApplicationController
 
   end
   
-
+	def twiter_keywords
+		c = SubjectKeyword.all_by_ep(EPISODE)
+		kw = ""
+		(1..c.count-1).each do |i|
+			kw += "\"#{c[i].keyword}\" OR "
+		end 
+		kw += %Q("#poop" OR "#denverwarbler")
+		return kw
+	end
+	
 	def update_weather (wocity)
 		client = Weatherman::Client.new :unit => "f"
 		client.lookup_by_woeid wocity
